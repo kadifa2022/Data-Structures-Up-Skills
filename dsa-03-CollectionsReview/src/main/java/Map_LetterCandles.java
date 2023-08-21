940fc4a20b3df74345127e20cefb4f475472915f
Kadifa <Kudickadifa@gmail.com>
2023-08-20 19:42:43 -0500

    practice

---
+++ dsa-03-CollectionsReview/src/main/java/Map_LetterCandles.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+
+public class Map_LetterCandles {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //Read inputs here!
+        System.out.println("Enter letter count: ");
+        int letterCount = Integer.parseInt(reader.readLine());
+
+        System.out.println("Enter number candles that can be removed: ");
+        int m = Integer.parseInt(reader.readLine());
+
+        System.out.println("Enter candle letter: ");
+        String letters = reader.readLine();
+
+        System.out.println(letterCandlesCost(letterCount, m, letters));
+
+    }
+
+    public static int letterCandlesCost(int letterCount, int m, String str) {
+        int cost = 0;
+        // create a hashMap
+        //O(n<26)
+        // character as Key value, Integer for frequency
+        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        //calculate frequency
+        //O(n)
+
+        for (Character ch : str.toCharArray()) {
+            if (map.containsKey(ch)) {
+                count = map.get(ch);
+                map.put(ch, count + 1);
+            } else map.put(ch, 1);
+        }
+        //nlogn n is not grater than 26
+        List<Integer> letterFreq = map.values().stream().sorted((o1, o2) -> o2.compareTo(o1)).collect(toList());
+        int index = 0;
+        //minimize cost
+        //O(n) n is not grater than 26 in fact it is constant
+        while (m > 0 && index <= letterFreq.size() - 1) {
+            if (m > 0 && letterFreq.size() == 1) {
+                letterFreq.set(0, letterFreq.get(0) - 1);
+                m--;
+            } else if (letterFreq.get(index) >= letterFreq.get(index + 1)) {
+                letterFreq.set(index, letterFreq.get(index) - 1);
+                m--;
+            } else if (index > 0 && letterFreq.get(index) < letterFreq.get(index - 1)) index--;
+            else index++;
+        }//O(m=26 at most) O(1)
+
+        // calculate min cost
+        //O(n) n is not >26
+        for (Integer num : letterFreq) {
+            cost += num * num;
+        }
+        //return resultO(n)
+        return cost;
+    }
+}
