e548e9ab28a40d909624569d683d6dcb19d3aaa1
Kadifa <Kudickadifa@gmail.com>
2023-08-24 11:41:32 -0500

    practice

---
+++ dsa-04-LinkedList/src/main/java/LikedList/UserListApp.java
@@ -0,0 +1,4 @@
+package LikedList;
+
+public class UserListApp {
+}
---
+++ dsa-04-LinkedList/src/main/java/LikedList/UserNode.java
@@ -0,0 +1,15 @@
+package LikedList;
+
+public class UserNode {
+
+    // this is my data for userNode class and address
+    String name;
+    String lastName;
+
+    UserNode next;
+
+    public UserNode(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
