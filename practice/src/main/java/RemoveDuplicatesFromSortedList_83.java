b57d6a58271b23d0a166943a1fe2cf1fb1c8e8a5
Kadifa <Kudickadifa@gmail.com>
2023-09-29 18:10:34 -0500

    practice

--- practice/src/main/java/RemoveDuplicatesFromSortedList_83.java
+++ practice/src/main/java/RemoveDuplicatesFromSortedList_83.java
@@ -1,2 +1,34 @@
+import com.sun.tools.javac.Main;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class RemoveDuplicatesFromSortedList_83 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+
+        System.out.println(removeDuplicates((ListNode) list));
+
+
+    }
+
+    public static ListNode removeDuplicates(ListNode head){
+        ListNode current = head;
+        while(current != null && current.next !=null){
+            if(current.next.val == current.val){
+                current.next = current.next.next;
+            }else{
+                current= current.next;
+            }
+        }
+        return head;
+
+    }
 }
