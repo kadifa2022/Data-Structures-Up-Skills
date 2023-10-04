3ca2bd8e1338f1689974e95a2c4f40458facca7d
Kadifa <Kudickadifa@gmail.com>
2023-10-04 05:55:18 -0500

    practice longest common prefix

--- practice/src/main/java/LengthOfLastString_58.java
+++ practice/src/main/java/LengthOfLastString_58.java
@@ -7,12 +7,12 @@ public class LengthOfLastString_58 {
     }
 
     public static int lengthOfLastWord(String s){
-        String str = s.trim();
+        String str = s.trim();// taking off-white spaces
          int count =0;
 
-         for(int i = str.length()-1; i>=0; i--){
+         for(int i = str.length()-1; i>=0; i--){// starting from the back loop until space
              if(str.charAt(i) !=' '){
-                 count++;
+                 count++; //increase count
              }else{
                  break;
              }
