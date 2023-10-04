76d2851a85f104271da1bd8a62d4e4d2e840d023
Kadifa <Kudickadifa@gmail.com>
2023-10-04 04:49:19 -0500

    practice longest common prefix

--- practice/src/main/java/TwoSum_II_With2Pointers_167.java
+++ practice/src/main/java/TwoSum_II_With2Pointers_167.java
@@ -5,19 +5,19 @@ public class TwoSum_II_With2Pointers_167 {
      public static int [] twoSumWith2Pointers(int [] numbers, int target){
 
         // define pointers
-         int L =0, n = numbers.length , R= 0;
+         int L =0, n = numbers.length , R = n-1;
          // two pointers find the sum
 
-         while(L < R){
-             int sum = numbers[L] + numbers [R];
-             if(sum == target){
-                 return new int [] {L +1, R+1};
-             }else if(sum< target){
-                 L++;
+         while(L < R){ // while left is less than right
+             int sum = numbers[L] + numbers [R]; // defining sum-> sum is equals numbers on L and numbers on R// o
+             if(sum == target){// once we have sum, we will  see if is sum equals to target
+                 return new int [] {L +1, R+1};//if is equal we are returning positions on  0 element
+             }else if(sum< target){// if is less than target
+                 L++;//we are moving left pointer by 1
              }else{
-                 R++;
+                 R--;// we are moving right pointer by 1 to the left
              }
          }
-         return new int[] {-1,-1};
+         return new int[] {-1,-1};//if don't find solution we are returning negative one, negative one
      }
 }
