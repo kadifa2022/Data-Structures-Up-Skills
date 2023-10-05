a49d87f86dae266bc51ffe0c251e279fca73c872
Kadifa <Kudickadifa@gmail.com>
2023-10-05 18:45:36 -0500

    practice longest common prefix

---
+++ practice/src/main/java/ListNode1.java
@@ -0,0 +1,10 @@
+public class ListNode1 {// Definition for Singly Linked list
+
+    int val;
+    ListNode1 next;
+    ListNode1() {}
+    ListNode1(int val){this.val = val;}
+    ListNode1(int val, ListNode1 next) {this.val = val; this.next = next;}
+
+
+}
---
+++ practice/src/main/java/MergeTwoSortedList_21.java
@@ -0,0 +1,42 @@
+
+
+public class MergeTwoSortedList_21 {
+
+    public static void main(String[] args) {
+
+
+
+
+        // mergeTwoLists(list1, list2)
+
+    }
+
+    public  ListNode1 mergeTwoLists(ListNode1 list1, ListNode1 list2) {
+        // base
+
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode1 head;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2= list2.next;
+        }
+        head.next = mergeTwoLists(list1, list2);
+        return head;
+
+
+    }
+
+
+
+
+}
