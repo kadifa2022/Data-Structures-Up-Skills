6a46291b48ceda7e14b872f972845e005cbdf75b
Kadifa <Kudickadifa@gmail.com>
2023-10-10 07:58:25 -0500

    practice longest common prefix

--- practice/src/main/java/ClimbingStairs_70.java
+++ practice/src/main/java/ClimbingStairs_70.java
@@ -1,26 +1,44 @@
 public class ClimbingStairs_70 {
 
     public static void main(String[] args) {
-        System.out.println(climbingStairs(3));
+        System.out.println(climbingStairs2(6));
 
     }
 
-    public static int climbingStairs(int n) {
+    public static int climbingStairs2(int n) {
         if (n == 1) {
             return n;
         }
-        int[] steps = new int[n + 1];
+        int step1 = 1;
+        int step2 = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = step2;
+            step2 = step1 + step2;
+            step1 = temp;
+
+        }
+      return step2;
+
+    }
+}
+
+/*
+    public static int climbingStairs(int n) {// dynamic programing
+        if (n == 1) {
+            return n;
+        }//Extra arr and cos us O(n) space complexity
+        int[] steps = new int[n + 1];// initialing an array for dynamic prog.
         steps[1] = 1;
         steps[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            steps[i] = steps[i - 1] + steps[i - 2];
+        for (int i = 3; i <= n; i++) { // from num3 we calculate las two steps all a way to the last number
+            steps[i] = steps[i - 1] + steps[i - 2]; // we can jump 1 or 2 steps
 
         }
         return steps[n];
 
     }
 }
-/*
+
 You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps.
@@ -41,4 +59,8 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 
+first 2 steps are sam for 3,  we can just add up one more// we combined fist 2 solutions=> stairs[3]=(1+1)+1=3//stairs[3] = 2+1 =3
+to reach stars 4 we =>stairs[4] = 3+2 =5; using logic (adding up last two steps) stairs [5] = 5 + 3 =8; stairs[6] = 5 + 8 =13;
+stairs = 1,2,3,4,5,6
+steps => 1=1, 2=2, 3=3, 4=5, 5=8, 6=13 distinct ways
  */
