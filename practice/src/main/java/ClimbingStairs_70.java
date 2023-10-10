0be32a676e4a654dbf10488638dd9b14bbc16a06
Kadifa <Kudickadifa@gmail.com>
2023-10-10 06:45:04 -0500

    practice longest common prefix

---
+++ practice/src/main/java/ClimbingStairs_70.java
@@ -0,0 +1,44 @@
+public class ClimbingStairs_70 {
+
+    public static void main(String[] args) {
+        System.out.println(climbingStairs(3));
+
+    }
+
+    public static int climbingStairs(int n) {
+        if (n == 1) {
+            return n;
+        }
+        int[] steps = new int[n + 1];
+        steps[1] = 1;
+        steps[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+
+        }
+        return steps[n];
+
+    }
+}
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps.
+In how many distinct ways can you climb to the top?
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+ */
