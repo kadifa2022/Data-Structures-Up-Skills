c3d51cdaa5b23a76a18bdd6cc37b78194a52c788
Kadifa <Kudickadifa@gmail.com>
2024-05-07 20:32:11 -0500

    practice longest common prefix

--- practice/src/main/java/MergeSortedArray_LeetCode_88.java
+++ practice/src/main/java/MergeSortedArray_LeetCode_88.java
@@ -3,24 +3,33 @@ import java.util.Arrays;
 public class MergeSortedArray_LeetCode_88 {
 
     public static void main(String[] args) {
-        int [] nums1= {1,2,3,0,0,0};
-        int [] nums2={2,5,6};
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int p1 = 3;
+        int p2 = nums2.length;
 
+        int[] mergedArray = merge(nums1, p1, nums2, p2);
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
 
-
-    }
-
-    public static void merge(int[] nums1,int m,int [] nums2, int n ){
-
-        int count = 0;
-
-        while(m<nums1.length){
-            nums1[m] = nums2[count];
-            count++;
-            m++;
         }
-        Arrays.sort(nums1);
+    }
+    public static int [] merge(int[] nums1,int m,int [] nums2, int n ) {
+
+        int p1 = m - 1, p2 = n - 1, i = m + n - 1;
+
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[i--] = nums1[p1--];
+            } else {
+                nums1[i--] = nums2[p2--];
+
+            }
+        }
+        return nums1;
+
 
     }
+
 
 }
