0cbe94666f2d7378f885c16510bba637d539cf26
Kadifa <Kudickadifa@gmail.com>
2024-05-24 09:10:50 -0500

    linked list practice

---
+++ dsa-04-LinkedList/src/main/java/ImplementationOfLinkedList/UserNode.java
@@ -0,0 +1,13 @@
+package ImplementationOfLinkedList;
+
+public class UserNode {
+    String name;
+    String lastName;
+    UserNode next;
+
+    public UserNode (String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+
+    }
+}
