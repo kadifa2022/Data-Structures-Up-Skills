a18f240f0adc7f21362af295479273e66a49d65e
Kadifa <Kudickadifa@gmail.com>
2024-05-24 19:33:34 -0500

    linked list practice

--- dsa-04-LinkedList/src/main/java/ImplementationOfLinkedList/UserLinkedList.java
+++ dsa-04-LinkedList/src/main/java/ImplementationOfLinkedList/UserLinkedList.java
@@ -25,19 +25,52 @@ public class UserLinkedList {
         }
         size ++ ;
     }
-    void printNames(){
+
+    void printNames() {
         // if is empty print list is empty
-        if(isEmpty()) {
+        if (isEmpty()) {
             System.out.println("List is empty");
             return;
         }
         // Assign current value to the head
-        UserNode current= head;
+        UserNode current = head;
         //loop
-        while (current != null){
-
+        while (current != null) {
+            System.out.print(current.name + " => ");
+            current = current.next;
+            if (current == null) System.out.print("null");
         }
+    }
 
+    void deleteByName(String name) {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+        UserNode prev = head;
+        UserNode current = head;
+
+        while (current != null) {
+            if (current.name.equals(name)) {// found and delete it
+                //case 1: head
+                if (current == head) {
+                    if (head == tail) tail = null;
+                    head = current.next;
+                    current.next = null; // break reference from current
+                }
+                //case 2: tail
+                if (current == tail) {
+                    prev.next = null;
+                    tail = prev;
+                } else {// case 3: middle
+                    prev.next = current.next;
+                    current.next = null;
+                }
+                size--;
+            }
+        }
+        prev = current;
+        current = current.next;
     }
 
 }
--- dsa-04-LinkedList/src/main/java/ImplementationOfLinkedList/UserListApp.java
+++ dsa-04-LinkedList/src/main/java/ImplementationOfLinkedList/UserListApp.java
@@ -3,5 +3,12 @@ package ImplementationOfLinkedList;
 public class UserListApp {
     public static void main(String[] args) {
 
+        UserLinkedList list = new UserLinkedList();
+        list.insertLast(new UserNode("Carol", "Smith"));
+        list.insertLast(new UserNode("Jack", "Reacher"));
+        list.insertLast(new UserNode("Tina", "Blay"));
+        list.printNames();
+
+
     }
 }
