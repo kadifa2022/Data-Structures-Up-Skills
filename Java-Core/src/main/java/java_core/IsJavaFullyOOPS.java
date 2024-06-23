9cde8db0876fd1090ab04d8f2f1e6f1234f48b61
Kadifa <Kudickadifa@gmail.com>
2024-06-23 17:14:59 -0500

    linked list practice

---
+++ Java-Core/src/main/java/java_core/IsJavaFullyOOPS.java
@@ -0,0 +1,23 @@
+package java_core;
+
+public class IsJavaFullyOOPS {
+    public static void main(String[] args) {
+        int i =10;
+        byte b= 20;
+        short s = 30;
+        long l = 100L;
+        double d = 100.10;
+        float f = 200f;
+        boolean flag = true;
+        char ch = 'R';
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(flag);
+        System.out.println(ch);
+
+    }
+}
