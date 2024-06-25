6acffa8304a241eaf413ecbeab3b44f02f198246
Kadifa <Kudickadifa@gmail.com>
2024-06-24 22:25:39 -0500

    linked list practice

--- dsa-03-CollectionsReview/src/main/java/collectionsReview/ArrayListReview.java
+++ dsa-03-CollectionsReview/src/main/java/collectionsReview/ArrayListReview.java
@@ -9,7 +9,7 @@ public class ArrayListReview {
     public static void main(String[] args) {
         //Convert an Array to ArrayList with Arrays.asList()
         // two different ways to convert array  to list
-        Integer [] arr = {1,2,3,4,5,5};
+        Integer [] arr = {4,5,5,8,7,3,6,1,2,3};
         List<Integer> list = Arrays.asList(arr);
         ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(arr));
 
@@ -24,5 +24,12 @@ public class ArrayListReview {
         Integer [] arr2 = nums.toArray( new Integer[0]);
         //converting an ArrayList to Array with stream()
         int[] arr3 = nums.stream().mapToInt(i->i).toArray();
+
+        //sort without comparator
+        Collections.sort(list2);// O(n log n)
+        System.out.println(list2);
+
+        //Binary search (must be sorted in ascending order) on collection performance (O (log n)
+
     }
 }
---
+++ dsa-03-CollectionsReview/src/main/java/collectionsReview/MapReview2.java
@@ -0,0 +1,37 @@
+package collectionsReview;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapReview2 {
+    public static void main(String[] args) {
+        Map<Integer, String> studentMap = new HashMap<>();
+        studentMap.put(1,"Jack");
+        studentMap.put(2,"Julia");
+        studentMap.put(3,"Alison");
+        studentMap.put(4,"Jack2");
+
+        // how to get values from a map
+        List<String > list =  studentMap.values().stream().collect(Collectors.toList());
+
+        // How  to get the value and sort them
+        List<String> list2 = studentMap.values().stream()
+                .sorted((o1,o2)->o1.compareToIgnoreCase(o2)).collect(Collectors.toList());
+        System.out.println(list2);
+
+        //how to get key and value EntrySet()
+        Set<Map.Entry<Integer, String>> entrySet = studentMap.entrySet();
+        System.out.println(entrySet);
+
+        // how to get keySet()
+        Set<Integer> keySet = studentMap.keySet();
+        System.out.println(keySet);
+
+
+
+
+
+
+
+    }
+}
