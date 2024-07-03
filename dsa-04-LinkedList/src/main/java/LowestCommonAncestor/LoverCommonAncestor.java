d4d60a5bd70f309efb7ff544a8250b11eae1a2a0
Kadifa <Kudickadifa@gmail.com>
2024-07-02 19:22:14 -0500

    linked list practice

---
+++ dsa-04-LinkedList/src/main/java/LowestCommonAncestor/LoverCommonAncestor.java
@@ -0,0 +1,20 @@
+package LowestCommonAncestor;
+
+public class  LoverCommonAncestor {
+    public static void main(String[] args) {
+
+       // System.out.println(lowestCommonAncestor());
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left, p,q);
+        }
+        if(p.val> root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p,q);
+        }
+        return root;
+    }
+}
