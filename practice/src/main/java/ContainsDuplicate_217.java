8bbb68508f4dadd86ae82cee64fed837bb2aa115
Kadifa <Kudickadifa@gmail.com>
2024-09-27 18:03:44 -0500

    contains duplicate

--- practice/src/main/java/ContainsDuplicate_217.java
+++ practice/src/main/java/ContainsDuplicate_217.java
@@ -10,14 +10,13 @@ public class ContainsDuplicate_217 {
     public static boolean containsDuplicate(int [] nums){
         Set<Integer> set = new HashSet<>();
 
-        for(int i =0; i <nums.length; i++){
+    for(int i =0; i <nums.length; i++){
             if (set.contains(nums[i])){
                 return true;
             }
             set.add(nums[i]);
+    }
 
-        }
-
-        return false;
+    return false;
     }
 }
