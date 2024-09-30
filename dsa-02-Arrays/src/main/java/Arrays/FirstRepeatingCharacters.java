bfa92d87faed9de9855c7f779bd5eafd0108d983
Kadifa <Kudickadifa@gmail.com>
2024-09-30 10:32:18 -0500

    first repeated character

--- dsa-02-Arrays/src/main/java/Arrays/FirstRepeatingCharacters.java
+++ dsa-02-Arrays/src/main/java/Arrays/FirstRepeatingCharacters.java
@@ -1,4 +1,28 @@
 package Arrays;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class FirstRepeatingCharacters {
+    public static void main(String[] args) {
+        Set<Student>  set = new LinkedHashSet<>();
+        set.add(new Student(1, "Jack"));
+        set.add(new Student(2, "Mike"));
+        set.add(new Student(3, "Sara"));
+        set.add(new Student(4, "Hana"));
+        set.add(new Student(5, "Joe"));
+
+    }
+    public static Character firstRepeatingCharacter(String str){
+        Set<Character> chars = new HashSet<>();
+        for(Character ch: str.toCharArray()){
+            if(!chars.add(ch)) return ch;
+        }
+        return null;
+
+
+
+    }
+
 }
