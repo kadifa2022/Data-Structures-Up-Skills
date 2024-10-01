2c19dba1645369c692c912ab81fee138cddff925
Kadifa <Kudickadifa@gmail.com>
2024-10-01 05:37:09 -0500

    Linked list review

---
+++ dsa-04-LinkedList/src/main/java/Review_LikedList/Node.java
@@ -0,0 +1,11 @@
+package Review_LikedList;
+
+public class Node {
+
+    int id;
+    Node next;// next variable will contain address of car clas object
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
