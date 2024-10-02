c69975e3d42fe394289c52f90b205fd8c2427ead
Kadifa <Kudickadifa@gmail.com>
2024-10-02 09:34:40 -0500

    Linked list review

--- dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList.java
+++ dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList.java
@@ -24,7 +24,53 @@ public class UserLinkedList {
             tail = node;
         }
         size++;
+    }
+    void insertFirst(UserNode node){
+        // case1: if list is empty
+        if(isEmpty()){
+            head =tail = node;
+        }else{// if is not empty
+            node.next = head;
+            head = node;
+        }
+        size++;
+    }
 
+
+    void printNames(){
+        // first check if list is empty
+        if(isEmpty()){
+            System.out.println("List is empty");
+            return;
+        }
+        //go to the first element in the list
+        UserNode current = head;
+        System.out.println();
+        while(current!=null){
+            System.out.print(current.name + " => ");
+            current = current.next;
+            if(current == null) System.out.print("null");
+
+
+        }
 
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
