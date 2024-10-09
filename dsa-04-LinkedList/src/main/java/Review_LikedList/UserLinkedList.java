83efc5f8ab243efb598d7dfcf91e109d90314d48
Kadifa <Kudickadifa@gmail.com>
2024-10-09 01:10:32 -0500

    Linked list review

--- dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList.java
+++ dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList.java
@@ -37,7 +37,7 @@ public class UserLinkedList {
     }
 
 
-    void printNames(){
+    void printNames() {
         // first check if list is empty
         if(isEmpty()){
             System.out.println("List is empty");
@@ -46,14 +46,42 @@ public class UserLinkedList {
         //go to the first element in the list
         UserNode current = head;
         System.out.println();
-        while(current!=null){
+        while (current != null) {
             System.out.print(current.name + " => ");
             current = current.next;
-            if(current == null) System.out.print("null");
+            if (current == null) System.out.print("null");
 
 
         }
+    }
+    void deleteByName(String name){
+        if(isEmpty()) {// check if list is empty
+            System.out.println("List is empty");
+        }
+        //if is not empty continue -> assign 2 node current and prev
+        UserNode current=head;
+        UserNode prev =head;
 
+        while (current != null){
+            if (current.name.equals(name)){
+                //we have to handle 3 cases
+                //1. head
+                if(current==head){
+                    if(current==tail) tail = null;
+                    head = current.next; //null
+                    current.next= null;
+
+                } else if (current==tail) {
+
+                }else{
+                }
+                size--;
+
+
+            }
+            prev=current;
+            current=current.next;
+        }
     }
 
 }
