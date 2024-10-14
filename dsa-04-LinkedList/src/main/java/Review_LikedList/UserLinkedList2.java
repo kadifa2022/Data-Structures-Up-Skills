64c681bcc10641190db1676e7ae1581348d9e246
Kadifa <Kudickadifa@gmail.com>
2024-10-14 01:56:46 -0500

    Linked list review

--- dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList2.java
+++ dsa-04-LinkedList/src/main/java/Review_LikedList/UserLinkedList2.java
@@ -4,42 +4,78 @@ public class UserLinkedList2 {
     UserNode head;
     UserNode tail;
     int size;
-    public UserLinkedList2(){
+
+    public UserLinkedList2() {
         this.size = 0;
     }
-    boolean isEmpty(){
+
+    boolean isEmpty() {
         return head == null;
     }
-    void insertLast(UserNode node){
-        if(isEmpty()){
-            head=tail=node;
-        }else{
+
+    void insertLast(UserNode node) {
+        if (isEmpty()) {
+            head = tail = node;
+        } else {
             tail.next = node;
             tail = node;
         }
         size++;
     }
-    void insertFirst(UserNode node){
-        if(isEmpty()){
+
+    void insertFirst(UserNode node) {
+        if (isEmpty()) {
             head = tail = node;
-        }else{
+        } else {
             node.next = head;
             head = node;
         }
-        size ++;
+        size++;
     }
-    void printNames(){
-        if(isEmpty()){
+
+    void printNames() {
+        if (isEmpty()) {
             System.out.println("List is empty");
             return;
         }
         UserNode current = head;
         System.out.println();
-        while(current != null){
+        while (current != null) {
             System.out.print(current.name + " => ");
             current = current.next;
-            if(current == null) System.out.println("null");
+            if (current == null) System.out.println("null");
         }
-
     }
+
+    void deleteByName(String name) {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+        }
+        UserNode current = head;
+        UserNode prev = head;
+
+        while (current != null) {
+            if (current.name.equals(name)) {
+                if (current == head) {// if is only one element
+                    if (current == tail) tail = null;
+                    head = current.next;
+                    current.next = null;
+                }else if(current == tail){
+                    prev.next = null;
+                    tail = prev;
+                } else {
+                    prev.next = current.next;
+                    current.next= null;
+                }
+                size--;
+
+
+
+            }
+            prev = current;
+            current = current.next;
+        }
+    }
+
+
 }
