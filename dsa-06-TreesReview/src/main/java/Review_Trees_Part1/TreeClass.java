8a2aa9761f2a340f96ccb72c5fb6de66c1ad2f05
Kadifa <Kudickadifa@gmail.com>
2024-10-26 06:01:38 -0500

    review trees

---
+++ dsa-06-TreesReview/src/main/java/Review_Trees_Part1/TreeClass.java
@@ -0,0 +1,10 @@
+package Review_Trees_Part1;
+
+public class TreeClass {
+// variables created from the classes are NULL by default.
+
+    TreeNode root;
+    public TreeClass() {// default constructor
+         // why default is null?  Reference types? null.
+    }
+}
--- dsa-06-TreesReview/src/main/java/Review_Trees_Part1/TreeNode.java
+++ dsa-06-TreesReview/src/main/java/Review_Trees_Part1/TreeNode.java
@@ -1,4 +1,22 @@
 package Review_Trees_Part1;
 
-public class TreeNode {
+public class TreeNode { // how to create tree node
+    // Data
+    String name;
+    String lastName;
+    int id;
+
+
+
+    TreeNode leftChild;   // reference for the left child
+    TreeNode rightChild;  //reference for the right child
+
+    public TreeNode(String name, String lastName, int id) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+
+
 }
