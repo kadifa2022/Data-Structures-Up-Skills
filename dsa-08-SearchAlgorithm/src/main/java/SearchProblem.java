448559172553830e0498bbe3a32e3db1e385e153
Kadifa <Kudickadifa@gmail.com>
2024-10-31 18:02:24 -0500

    Search Algorithm Binary search

---
+++ dsa-08-SearchAlgorithm/src/main/java/SearchProblem.java
@@ -0,0 +1,26 @@
+public class SearchProblem {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,2,4,5,6,7,8};
+        System.out.println(search(nums, 9));
+
+    }
+
+    public static  int search(int[] array, int data) {
+        // initialize left and right
+        int left = 0;
+        int right = array.length-1;
+        // have while loop(left<=right)
+        while(left<=right) {
+            //calculate mid
+            int mid = (left + right) /2;
+            // compare == mid then return, mid index
+            if(data == array[mid]) return mid;
+            //else if data <mid then right =mid-1
+            else if(data<=array[mid]) right = mid-1;
+            //else left = mid+1
+            else left = mid +1;
+        }
+        return -1;
+
+    }
+}
