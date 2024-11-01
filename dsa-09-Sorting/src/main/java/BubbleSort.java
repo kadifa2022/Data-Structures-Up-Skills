2f5ad4b85a4a71652177cc776e2b21584db3bf47
Kadifa <Kudickadifa@gmail.com>
2024-11-01 06:40:01 -0500

    Sorting Algorithm

---
+++ dsa-09-Sorting/src/main/java/BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort {
+    public static int [] bubbleSort(int[]array){
+        for(int i = 0; i< array.length;i++){
+            boolean swap = false;
+            for(int j = 0; j< array.length-1; j++){
+                if(array[j]>array[j+1]){
+                    swap =true;
+                }
+            }
+            if(!swap) return array; // this mean array is in order no more swapping
+            }
+            return array;
+    }
+    public static void swap(int []array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+}
