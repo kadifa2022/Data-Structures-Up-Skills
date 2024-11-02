716c4c0b642108fb68a5efb2a5149c7234796a84
Kadifa <Kudickadifa@gmail.com>
2024-11-01 20:54:17 -0500

    Insertion sort impl

--- dsa-09-Sorting/src/main/java/InsertionSort.java
+++ dsa-09-Sorting/src/main/java/InsertionSort.java
@@ -1,12 +1,12 @@
-public class InsertionSort {
+public class InsertionSort { // the size of input data is reduced at each step
 
     public static int [] insertionSort(int[] array){
-        for(int i = 1; i< array.length; i++){
-            int toBeInserted = array[i]; // temp var toBeInserted
-            int j = i-1; // this is predecessor
+        for(int i = 1; i< array.length; i++){// iterate from array[1] to array[N] over the array
+             int toBeInserted = array[i]; // temp var toBeInserted// save ith value
+            int j = i-1; // this is predecessor // start from i-1 (predecessor)
             // start shifting operation here
-            while (j>= 0 && toBeInserted <array[j]){
-                array[j+1] = array[j];
+            while (j>= 0 && toBeInserted <array[j]){ // go back and compare until ith< array[j]
+                array[j+1] = array[j];// shifting operation here
                 j--;
 
             }
