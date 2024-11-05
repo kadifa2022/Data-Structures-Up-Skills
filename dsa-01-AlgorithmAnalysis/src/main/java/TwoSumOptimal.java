98db2768d1dce042d6f47d6547df1bf2eee7a82e
Kadifa <Kudickadifa@gmail.com>
2024-11-05 09:22:21 -0600

    Map review two sum optimal solution

--- dsa-01-AlgorithmAnalysis/src/main/java/TwoSumOptimal.java
+++ dsa-01-AlgorithmAnalysis/src/main/java/TwoSumOptimal.java
@@ -1,16 +1,17 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSumOptimal {
     public static void main(String[] args) {
         int [] arr = new int[]{2,7,9,11};
-        System.out.println(twoSumOptimalSolution(arr, 9));
+        System.out.println(Arrays.toString(twoSumOptimalSolution(arr, 9)));
 
     }
 
     public static int [] twoSumOptimalSolution(int [] array, int targetValue){
         // create hashMap
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();// o(n) complexity but i have space complexity O(n)
 
         for(int i = 0; i< array.length; i++){
             int potentialMatch = targetValue - array[i];
