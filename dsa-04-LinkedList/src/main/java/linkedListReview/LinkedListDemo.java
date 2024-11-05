e32648f3178ca6b78fb95e895b2303d1086b93e6
Kadifa <Kudickadifa@gmail.com>
2024-11-05 14:56:47 -0600

    LinkedList review

---
+++ dsa-04-LinkedList/src/main/java/linkedListReview/LinkedListDemo.java
@@ -0,0 +1,10 @@
+package linkedListReview;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        System.out.println(node1);
+        System.out.println("Next address is : " + node1.next);
+    }
+
+}
