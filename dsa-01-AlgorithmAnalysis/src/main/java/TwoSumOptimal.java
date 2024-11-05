38088a1c5dca236b80dce7b0d1fe8bb69c44aed6
Kadifa <Kudickadifa@gmail.com>
2024-11-05 09:10:41 -0600

    Map review two sum optimal solution

---
+++ dsa-01-AlgorithmAnalysis/src/main/java/TwoSumOptimal.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumOptimal {
+    public static void main(String[] args) {
+        int [] arr = new int[]{2,7,9,11};
+        System.out.println(twoSumOptimalSolution(arr, 9));
+
+    }
+
+    public static int [] twoSumOptimalSolution(int [] array, int targetValue){
+        // create hashMap
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i< array.length; i++){
+            int potentialMatch = targetValue - array[i];
+            if(map.containsKey(potentialMatch)) return new int[]{i,map.get(potentialMatch)};
+            else map.put(array[i], i);
+        }
+
+        return new int [] {};
+
+    }
+
+}
