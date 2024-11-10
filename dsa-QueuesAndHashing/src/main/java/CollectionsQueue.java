a58c4b26c8e9236ddf2d1e8c70b4832962d83eb0
Kadifa <Kudickadifa@gmail.com>
2024-11-09 18:33:30 -0600

    review queue

--- dsa-QueuesAndHashing/src/main/java/CollectionsQueue.java
+++ dsa-QueuesAndHashing/src/main/java/CollectionsQueue.java
@@ -1,5 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CollectionsQueue {
     public static void main(String[] args) {
+
+        MyQueue<Integer> mq = new MyQueue<>();
+        mq.enqueue(1);
+        mq.enqueue(2);
+        mq.enqueue(3);
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        Queue<Integer> collQ= new LinkedList<>();
+        collQ.add(5);
+        collQ.add(6);
+        System.out.println(collQ.remove());
+
 
     }
 }
