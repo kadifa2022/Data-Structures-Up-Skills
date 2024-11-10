2db654c44cd4b34ff6e4023e9a04182d660b72c5
Kadifa <Kudickadifa@gmail.com>
2024-11-09 19:36:04 -0600

    review queue

--- dsa-QueuesAndHashing/src/main/java/ReverseFirstKElementsQueueProblem.java
+++ dsa-QueuesAndHashing/src/main/java/ReverseFirstKElementsQueueProblem.java
@@ -31,7 +31,6 @@ public class ReverseFirstKElementsQueueProblem {
         for(int i = 0; i < queue.size - k; i ++){
             queue.enqueue(queue.dequeue());
         }
-
         return queue;
 
     }
